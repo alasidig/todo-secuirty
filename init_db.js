a542c9a2c2774de641ec9d5de7fbe6953342cac7
alasidig <ala.sidig@gmail.com>
2022-11-16 07:58:57 +0300

    SQL injection your passwords",(select GROUP_CONCAT(PASSWORD) from users));--

--- init_db.js
+++ init_db.js
@@ -13,12 +13,29 @@ user text not null,
 content text not null,
 done integer default 0 -- SqLite doesn't have a boolean type
 )`)
+    await db.exec(`create table if not exists users(
+id integer primary key autoincrement, 
+username text not null unique,
+password text not null
+)`)
+
 
     // 3. insert records in the table note true is treated as 1
-    const info = await db.run(`insert into tasks('user','content','done') 
-values  ('Ali','We have so many modules',0),
-        ('Ali','Good sleep for better achievement',true),
-        ('Ali','Keep healthy body',false)`)
-    console.log(info)
+    try {
+        const info = await db.run(`insert into tasks('id','user','content','done') 
+    values  (1,'Ali','We have so many modules',0),
+            (2,'Ali','Good sleep for better achievement',true),
+            (3,'Ali','Keep healthy body',false)`)
+        console.log(info)
+    } catch (e) {
+    }
+    try {
+         await db.run(`insert into users ('id','username','password') 
+    values  (1,'ali','abc123'),
+            (2,'khalid','123456')`)
+
+    }catch (e) {
+        console.log(e)
+    }
 }
 init().then()
